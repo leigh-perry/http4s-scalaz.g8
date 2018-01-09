a2d12c2170ae7525cc202b2acf5e4ce236d37064
lperry <lperry@simplemachines.com.au>
2018-01-09 15:06:23 +1100

    master: Cosmetic

--- src/main/g8/shared/src/test/scala/$package__packaged$/shared/testsupport/UtestScalaCheck.scala
+++ src/main/g8/shared/src/test/scala/$package__packaged$/shared/testsupport/UtestScalaCheck.scala
@@ -5,7 +5,6 @@ import org.scalacheck.{Prop, Test}
 import utest.assert
 
 trait UtestScalaCheck {
-
   protected[this] object UtestReporter extends Test.TestCallback {
     override def onTestResult(name: String, res: org.scalacheck.Test.Result) = {
       val scalaCheckResult = if (res.passed) "" else Pretty.pretty(res, Pretty.defaultParams)
@@ -14,9 +13,7 @@ trait UtestScalaCheck {
   }
 
   implicit protected[this] class PropWrapper(prop: Prop) {
-    def check(): Unit = {
-      prop.check(Test.Parameters.default.withTestCallback(UtestReporter))
-    }
+    def check(): Unit = prop.check(Test.Parameters.default.withTestCallback(UtestReporter))
   }
 
 }
