03da05092cee93e1b13dfbca6c4db4ad3fc7c926
lperry <lperry@simplemachines.com.au>
2018-03-15 10:35:37 +1100

    master: Remove unused newtypes

--- src/main/g8/shared/src/main/scala/$package__packaged$/shared/Newtype.scala
+++ src/main/g8/shared/src/main/scala/$package__packaged$/shared/Newtype.scala
@@ -3,17 +3,11 @@ package $package$.shared
 import $package$.shared.tagger.Tagger
 
 object Newtype {
-  type AmqpEndpoint = AmqpEndpoint.Type
-  object AmqpEndpoint extends Tagger[String]
-
   type UserName = UserName.Type
   object UserName extends Tagger[String]
 
   type Password = Password.Type
   object Password extends Tagger[String]
-
-  type TopicName = TopicName.Type
-  object TopicName extends Tagger[String]
 
   type PortNumber = PortNumber.Type
   object PortNumber extends Tagger[Int]
